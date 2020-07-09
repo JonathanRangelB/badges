a4aea3523ce53562f561097b24e8208aad45bebc
Jonathan Rangel <jonathanrangelb@gmail.com>
2020-07-08 21:48:59 -0500

    Implementando correctamente las páginas Home y 404

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import Badges from '../pages/Badges'
 import BadgeNew from '../pages/BadgeNew'
 import NotFound from '../pages/NotFound'
 import Layout from './Layout'
+import Home from '../pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
     <Layout>
       <Switch>
-        {/* <Route exact path="/" component={Home}></Route> */}
+        <Route exact path="/" component={Home}></Route>
         <Route exact path="/Badges" component={Badges}></Route>
         <Route exact path="/Badges/New" component={BadgeNew}></Route>
         <Route exact path="/404" component={NotFound}></Route>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import './styles/NotFound.css'
+import image404 from '../images/error404.svg'
 
 function NotFound() {
-    return (
-        <React.Fragment>
-            Not found
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div className="image-container">
+        <img className="image404" src={image404} alt="not found" />
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default NotFound
+export default NotFound;
