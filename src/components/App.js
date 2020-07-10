4f275fd6148d8df4f255df79f9b4e02c297adb7a
Jonathan Rangel <jonathanrangelb@gmail.com>
2020-07-10 18:16:34 -0500

    Implementacion de las diferentes fases del ciclo de vida de un componente a la pagina de badges

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Badges from '../pages/Badges'
 import BadgeNew from '../pages/BadgeNew'
 import NotFound from '../pages/NotFound'
@@ -9,16 +9,15 @@ import Home from '../pages/Home'
 function App() {
   return (
     <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/Badges" component={Badges}></Route>
-        <Route exact path="/Badges/New" component={BadgeNew}></Route>
-        <Route exact path="/404" component={NotFound}></Route>
-        <Redirect from='*' to='/404'/>
-        {/* <Route exact path="/Badges/New" component={BadgeNew}></Route> */}
-      </Switch>
-    </Layout>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/Badges" component={Badges}></Route>
+          <Route exact path="/Badges/New" component={BadgeNew}></Route>
+          <Route component={NotFound}></Route> {/* Puedo agregar exact y path junto con el BrowserRouter.Redirect para tener el mismo efecto */}
+          {/* <Redirect from='*' to='/404' /> */}
+        </Switch>
+      </Layout>
     </BrowserRouter>
   )
 }
